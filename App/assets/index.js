d278e5086921232460b8125ec29b566e2bdbb85a
KhairanRizqiansyah <khaimkr32@gmail.com>
2022-10-12 14:29:34 +0700

    membuat file index untuk assets

---
+++ App/assets/index.js
@@ -0,0 +1,30 @@
+import peduli from './img/peduli.png';
+import kpclogo from './img/logo_kpcpen.png';
+import kominfologo from './img/logo_kominfo.png';
+import kemenkeslogo from './img/logo_kemenkes.png';
+import bumnlogo from './img/logo_bumn.png';
+import toppeduli from './img/logo-top.png';
+import emptylogo from './img/ic_empty.jpg';
+import checkinlogo from './img/ic_check_in.jpg';
+import childlogo from './img/ic_child.jpg';
+import covidlogo from './img/ic_covid.jpg';
+import bgtopehac from './img/bg_ehac.jpg';
+import myehac from './img/ic_my_ehc.jpg';
+import crtehac from './img/ic_create_ehc.jpg';
+import lonceng from './img/lonceng.jpeg';
+import user from './img/user.jpeg';
+import panah from './img/panah.jpeg';
+import vaksin from './img/ic_vaccine_and_immuniazation.jpg';
+import covid from './img/ic_covid_test_result.jpg';
+import ehac from './img/ic_ehac.jpg';
+import tangan from './img/ic_hand_qr.jpg';
+import scan from './img/scan.jpeg';
+import travel from './img/ic_travel_regulation.jpg';
+import telemedicine from './img/ic_telemedicine.jpg';
+import healthcare from './img/ic_healthcare_facility.jpg';
+import statistik from './img/ic_statistic_covid.jpg';
+import hospital from './img/ic_find_hospital.jpg';
+
+export {peduli, kpclogo, kominfologo, kemenkeslogo, bumnlogo, toppeduli, emptylogo, checkinlogo, childlogo,
+covidlogo, bgtopehac, myehac, crtehac, lonceng, user, panah, vaksin, covid, ehac, tangan, scan, travel, 
+telemedicine, healthcare, statistik, hospital};
