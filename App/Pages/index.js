a4af6d49f7fc3f9223581d29e23451ba066564c7
KhairanRizqiansyah <khaimkr32@gmail.com>
2022-10-12 14:14:35 +0700

    memasukan script index.js pada pages

---
+++ App/Pages/index.js
@@ -0,0 +1,8 @@
+import HomeScreen from './HomeScreen';
+import SplashScreen from './SplashScreen';
+import LoginScreen from './LoginScreen';
+import CovidTestScreen from './CovidTestScreen';
+import ImunizationScreen from './ImunizationScreen';
+import EhacScreen from './EhacScreen';
+
+export {HomeScreen, SplashScreen, LoginScreen, CovidTestScreen, ImunizationScreen, EhacScreen};
